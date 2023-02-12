e580686770e12f8738b2c1df768e643e071448c0
lincw <56734434+lincww@users.noreply.github.com>
2023-02-12 22:54:26 +0800

    tmp save

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,34 @@
 import yayJpg from '../assets/yay.jpg';
 import useSWR from 'swr'
 import {Alert, Spin} from "antd";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const StarsStatistics = ():React.FC => {
+const StarsStatistics = (): JSX.Element => {
   return (
     <>
+      <div>test</div>
     </>
   )
 }
 
 export default function HomePage() {
-  const {data, error, isLoading} = useSWR('/', (...args) => fetch(...args).then(res => res.json()))
-  if (isLoading) return <Spin />
-  if (error) return <><Alert message="loading error" type="error" /> </>
+  const {data, error, isLoading} = useSWR('/api/list', (...args) => fetch(...args).then(res => res.json()))
+  if (isLoading) return <Spin/>
+  if (error) return <Alert message="loading error" type="error"/>
+
+  const {
+    data: historyData,
+    error: historyError,
+    isLoading: isHistoryLoading
+  } = useSWR('/api/history', (...args) => fetch(...args).then(res => res.json()))
+  const [displayedHistoryData, setDisplayedHistoryData] = useState(null)
+  useEffect(() => {
+    if (!isHistoryLoading) {
+      historyData.forEach((historys: any) => {
+      })
+    }
+  }, [historyData])
+
   return (
     <div>
       <div>
