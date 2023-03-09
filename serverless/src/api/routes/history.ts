30c86f199fc7d525786687347e9d7928947234da
lincw <56734434+lincww@users.noreply.github.com>
2023-03-09 15:10:59 +0800

    feat: serverless support history KV pair (#8)

--- serverless/src/api/routes/history.ts
+++ serverless/src/api/routes/history.ts
@@ -1,9 +1,9 @@
 const HistoryAction = async (req: Request, username: string, KV: KVNamespace): Promise<Response> => {
-  const latestStars = await KV.get(`latestStars:${username}`)
   const lists = await KV.list({prefix: `diff:${username}`})
   // TODO: As limitation is 1000, maybe it's better to change this by year
   let diffList = await Promise.all(lists?.keys.map(key => KV.get(key.name, 'json')))
-  return new Response(JSON.stringify(diffList))
+  let resultMap = [lists?.keys.map(key=>key.name), diffList]
+  return new Response(JSON.stringify(resultMap))
 }
 
 export {HistoryAction}
